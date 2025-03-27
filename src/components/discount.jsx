961c5127d2b471296ce47524220ec81e1a1fed84
Bigbilly-github <abiolatwitter97@gmail.com>
2025-03-27 09:59:28 +0100

    adding responsiveness to the discount component

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -4,13 +4,13 @@
 function Discount (){
     return(
         <>
-        <section className="w-[1440px] h-[530px] bg-[#000000] pt-[123px]">
-            <div className="w-[1440px] h-[407px] bg-[#E9C664] bg-[url('/src/svg/discount/img.svg')]  pt-[103px] bg-no-repeat bg-[90%_50%] ">
-            <div className="w-[541px] h-[200px] ml-[140px]">
-                <h1 className="font-fatface text-[70px] mb-0 tracking-[5%] w-[541px] ">
+        <section className="w-full h-[530px] bg-[#000000] pt-[123px]">
+            <div className="lg:w-[1440px] md:w-[1200px] sm:w-[1050px] h-[407px] bg-[#E9C664] sm:bg-[url('/src/svg/discount/img.svg')]    pt-[103px] bg-no-repeat bg-[90%_50%] ">
+            <div className="w-[541px] h-[200px] xl:ml-[75px] md:ml-[40px] ml-[30px] ">
+                <h1 className="font-fatface xl:text-[70px] sm:text-[50px] text-[30px] mb-0 tracking-[5%] w-[541px] ">
                     25% DISCOUNT
                 </h1>
-                <p className="w-[541px] h-[31px] font-inter   mb-[35px] text-[16px] text-[#000000]">
+                <p className="xl:w-[541px] w-[400px] xl:h-[31px] h-[50px] font-inter   mb-[35px] text-[16px] text-[#000000]">
                     Curabitur vulputate arcu odio, ac facilisis diam accumsan ut. Ut imperdiet et leo in vulputate. Sed eleifend lacus eu sapien sagittis impe
                 </p>
                 <button className="w-[153px]  hover:opacity-75 transition-normal cursor-pointer h-[50px] bg-[#000000] font-inter font-semibold text-[20px] text-[#E9C664]">
